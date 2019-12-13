3ee04563e1d53d843413ba1e9821b335c51908d8
Quinn Wilson <github@midworld.org>
2019-12-13 12:28:06 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ variable "username" {
 }
 
 provider "aws" { 
-  bucket         = "terraform-up-and-running-state"
   region         = "us-east-2"
 }
 
@@ -18,5 +17,25 @@ resource "aws_s3_bucket" "terraform_state" {
         sse_algorithm = "AES256"
       }
     }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "midworld-terraform-up-and-running-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-2"    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
   }
 }
