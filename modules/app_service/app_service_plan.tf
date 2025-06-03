dd4e7fe6c410f9f5ff4549646bbe1fef2a821d10
Quinn Wilson <quinn.wilson@nimblepros.com>
2025-06-03 13:38:51 -0500

    drop to free tier

--- modules/app_service/app_service_plan.tf
+++ modules/app_service/app_service_plan.tf
@@ -3,5 +3,5 @@ resource "azurerm_service_plan" "elf_service_plan" {
   resource_group_name = var.resource_group_name
   location            = "westus2"
   os_type            = "Linux"
-  sku_name           = "B1"    # Basic tier, you can adjust this based on your needs
+  sku_name           = "F1"    # Basic tier, you can adjust this based on your needs
 }
