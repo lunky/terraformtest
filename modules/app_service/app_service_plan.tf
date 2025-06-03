07b4dccd45d380d4e3891838fb00df97aef0580a
Quinn Wilson <quinn.wilson@nimblepros.com>
2025-06-03 13:58:21 -0500

    try 6

--- modules/app_service/app_service_plan.tf
+++ modules/app_service/app_service_plan.tf
@@ -3,5 +3,5 @@ resource "azurerm_service_plan" "elf_service_plan" {
   resource_group_name = var.resource_group_name
   location            = "westus2"
   os_type            = "Linux"
-  sku_name           = "D1"    # Changed to D1 (Shared tier)
+  sku_name           = "S1"
 }
