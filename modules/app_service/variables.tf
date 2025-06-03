3bf7f78a98bb7fa1662929414eaf2dc2b3c55321
Quinn Wilson <quinn.wilson@nimblepros.com>
2025-06-03 16:05:26 -0500

    add postgres_admin_password variable

--- modules/app_service/variables.tf
+++ modules/app_service/variables.tf
@@ -19,3 +19,9 @@ variable "resource_group_name" {
   type        = string
   description = "Name of the resource group"
 }
+
+variable "postgres_admin_password" {
+  description = "The administrator password for PostgreSQL server"
+  type        = string
+  sensitive   = true
+}
