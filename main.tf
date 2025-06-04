9c26fc6f8fd505ae8667c5b0d40041a1e90a2599
Quinn Wilson <quinn.wilson@nimblepros.com>
2025-06-04 12:15:25 -0500

    refactor: replace local resource group references with module outputs

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "network" {
   source              = "./modules/network"
   name                = local.vnet_name
   location            = var.location
-  resource_group_name = local.resource_group
+  resource_group_name = module.resource_group.name
   tags                = local.default_tags
 }
 
@@ -19,7 +19,7 @@ module "postgres" {
   name                = local.postgres_name
   location            = var.location
   admin_password      = var.postgres_admin_password
-  resource_group_name = local.resource_group
+  resource_group_name = module.resource_group.name
   tags                = local.default_tags
 }
 
@@ -27,7 +27,7 @@ module "app_service_plan" {
   source              = "./modules/app_service_plan"
   name                = local.app_service_plan_name
   location            = var.location
-  resource_group_name = local.resource_group
+  resource_group_name = module.resource_group.name
   tags                = local.default_tags
 }
 
@@ -35,7 +35,7 @@ module "web_app" {
   source              = "./modules/web_app"
   name                = local.web_app_name
   location            = var.location
-  resource_group_name = local.resource_group
+  resource_group_name = module.resource_group.name
   app_service_plan_id = module.app_service_plan.id
   tags                = local.default_tags
 }
@@ -43,7 +43,7 @@ module "web_app" {
 module "front_door" {
   source              = "./modules/front_door"
   name                = local.front_door_name
-  resource_group_name = local.resource_group
+  resource_group_name = module.resource_group.name
   waf_policy_id       = module.waf.id
   tags                = local.default_tags
 }
@@ -51,21 +51,21 @@ module "front_door" {
 module "waf" {
   source              = "./modules/waf"
   name                = local.waf_name
-  resource_group_name = local.resource_group
+  resource_group_name = module.resource_group.name
   tags                = local.default_tags
 }
 
 module "private_dns_zone" {
   source              = "./modules/private_dns_zone"
   name                = local.private_dns_zone_name
-  resource_group_name = local.resource_group
+  resource_group_name = module.resource_group.name
   tags                = local.default_tags
 }
 
 module "virtual_network_link" {
   source              = "./modules/virtual_network_link"
   name                = local.vnet_link_name
-  resource_group_name = local.resource_group
+  resource_group_name = module.resource_group.name
   dns_zone_name       = local.private_dns_zone_name
   virtual_network_id  = module.network.id
   tags                = local.default_tags
@@ -75,14 +75,14 @@ module "storage" {
   source              = "./modules/storage"
   name                = local.storage_account_name
   location            = var.location
-  resource_group_name = local.resource_group
+  resource_group_name = module.resource_group.name
   tags                = local.default_tags
 }
 
 module "action_group" {
   source              = "./modules/action_group"
   name                = local.action_group_name
-  resource_group_name = local.resource_group
+  resource_group_name = module.resource_group.name
   short_name          = "act"
   email_receiver      = "alerts@example.com"
   tags                = local.default_tags
@@ -92,6 +92,6 @@ module "log_analytics" {
   source              = "./modules/log_analytics"
   name                = local.log_analytics_workspace_name
   location            = var.location
-  resource_group_name = local.resource_group
+  resource_group_name = module.resource_group.name
   tags                = local.default_tags
 }
