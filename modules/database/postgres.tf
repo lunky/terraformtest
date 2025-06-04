6be57037acf92a2a7ba726710c6c84418d1bd2a5
Quinn Wilson <quinn.wilson@nimblepros.com>
2025-06-03 20:51:28 -0500

    update postgres instance to use Standard_B1ms SKU

--- modules/database/postgres.tf
+++ modules/database/postgres.tf
@@ -2,7 +2,7 @@ resource "azurerm_postgresql_flexible_server" "elf_postgres" {
   name                = "elf-dev-v2"
   resource_group_name = var.resource_group_name
   location            = "East US"
-  sku_name            = "B_Generation5_1"
+  sku_name            = "B_Standard_B1ms"
   storage_mb          = 32768  # 32GB converted to MB
   storage_tier        = "P4"
   auto_grow_enabled   = false
