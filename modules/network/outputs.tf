a58295e92270a5725d17b9e3ad341c9bd584c40b
Quinn Wilson <quinn.wilson@nimblepros.com>
2025-06-05 08:06:26 -0500

    add custom firewall rules and outputs for virtual network configuration

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -3,3 +3,6 @@
 output "id" {
   value = azurerm_virtual_network.this.id
 }
+output "name" {
+  value = azurerm_virtual_network.this.name
+}
--- modules/waf/main.tf
+++ modules/waf/main.tf
@@ -1,9 +1,51 @@
 resource "azurerm_cdn_frontdoor_firewall_policy" "this" {
   name                              = var.name
   resource_group_name               = var.resource_group_name
-  mode                              = "Prevention"
-  sku_name                          = "Standard_AzureFrontDoor"
+  sku_name                          = azurerm_cdn_frontdoor_profile.this.sku_name
+  enabled                           = true
+  mode                             = "Prevention"
   custom_block_response_status_code = 403
-  custom_block_response_body = base64encode("Access denied by WAF policy")
-  tags                              = var.tags
+  custom_block_response_body        = base64encode("Access denied by WAF policy")
+  tags                             = var.tags
+
+  custom_rule {
+    name                           = "AllowQuinn"
+    enabled                        = true
+    priority                       = 1
+    rate_limit_duration_in_minutes = 1
+    rate_limit_threshold          = 1000
+    type                          = "MatchRule"
+    action                        = "Allow"
+
+    match_condition {
+      match_variable     = "RemoteAddr"
+      operator           = "IPMatch"
+      negation_condition = false
+      match_values       = ["104.246.135.160"]
+    }
+  }
+
+  custom_rule {
+    name                           = "BlockAll"
+    enabled                        = true
+    priority                       = 2
+    rate_limit_duration_in_minutes = 1
+    rate_limit_threshold          = 1000
+    type                          = "MatchRule"
+    action                        = "Block"
+
+    match_condition {
+      match_variable     = "RemoteAddr"
+      operator           = "IPMatch"
+      negation_condition = false
+      match_values       = ["0.0.0.0/0", "::/0"]
+    }
+  }
+
+  # Keep a managed rule set for basic security
+  managed_rule {
+    type    = "DefaultRuleSet"
+    version = "1.0"
+    action  = "Block"
+  }
 }
