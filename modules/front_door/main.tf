31b747c4ff4ba2c8302415a27d93075bf215c1a5
Quinn Wilson <quinn.wilson@nimblepros.com>
2025-06-05 11:34:43 -0500

    implement full Azure Front Door configuration with origin group, firewall, route, and security policy; update outputs

--- modules/front_door/main.tf
+++ modules/front_door/main.tf
@@ -1,4 +1,3 @@
-# modules/front_door/main.tf
 resource "azurerm_cdn_frontdoor_profile" "this" {
   name                = "${var.name}-profile"
   resource_group_name = var.resource_group_name
@@ -11,38 +10,74 @@ resource "azurerm_cdn_frontdoor_endpoint" "this" {
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.this.id
   tags                     = var.tags
 }
-# --
 
-# # Additional Front Door configuration as needed
-# resource "azurerm_cdn_frontdoor_origin_group" "this" {
-#   name                     = "${var.name}-origin-group"
-#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.this.id
-#   session_affinity_enabled = false
-#
-#   load_balancing {
-#     sample_size                 = 4
-#     successful_samples_required = 3
-#   }
-# }
-#
-# resource "azurerm_cdn_frontdoor_origin" "this" {
-#   name                          = "${var.name}-origin"
-#   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.this.id
-#   enabled                       = true
-#
-#   host_name          = var.backend_host
-#   http_port          = 80
-#   https_port         = 443
-#   priority           = 1
-#   weight             = 1000
-# }
+resource "azurerm_cdn_frontdoor_origin_group" "this" {
+  name                     = "${var.name}-origin-group"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.this.id
+  session_affinity_enabled = false
+
+  load_balancing {
+    sample_size                 = 4
+    successful_samples_required = 3
+  }
+
+  health_probe {
+    protocol            = "Https"
+    request_type        = "GET"
+    interval_in_seconds = 100
+  }
+}
+
+resource "azurerm_cdn_frontdoor_origin" "this" {
+  name                          = "${var.name}-origin"
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.this.id
+  enabled                       = true
+
+  host_name          = var.backend_host
+  http_port          = 80
+  https_port         = 443
+  priority           = 1
+  weight             = 1000
+
+  certificate_name_check_enabled = true
+}
+
+resource "azurerm_cdn_frontdoor_route" "this" {
+  name                          = "${var.name}-route"
+  cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.this.id
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.this.id
+  cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.this.id]
+  enabled                       = true
+
+  forwarding_protocol    = "HttpsOnly"
+  patterns_to_match      = ["/*"]
+  supported_protocols    = ["Http", "Https"]
+  link_to_default_domain = true
+}
 
 resource "azurerm_cdn_frontdoor_firewall_policy" "this" {
   name                              = var.name
   resource_group_name               = var.resource_group_name
   mode                              = "Prevention"
   custom_block_response_status_code = 403
-  custom_block_response_body = base64encode("Access denied by WAF policy")
+  custom_block_response_body        = base64encode("Access denied by WAF policy")
   sku_name                          = "Standard_AzureFrontDoor"
   tags                              = var.tags
+}
+
+resource "azurerm_cdn_frontdoor_security_policy" "this" {
+  name                     = "${var.name}-security-policy"
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.this.id
+
+  security_policies {
+    firewall {
+      cdn_frontdoor_firewall_policy_id = azurerm_cdn_frontdoor_firewall_policy.this.id
+      association {
+        patterns_to_match = ["/*"]
+        domain {
+          cdn_frontdoor_domain_id = azurerm_cdn_frontdoor_endpoint.this.id
+        }
+      }
+    }
+  }
 }
--- modules/front_door/outputs.tf
+++ modules/front_door/outputs.tf
@@ -2,9 +2,15 @@
 output "endpoint" {
   value = azurerm_cdn_frontdoor_endpoint.this.host_name
 }
+
 output "id" {
   value = azurerm_cdn_frontdoor_firewall_policy.this.id
 }
+
 output "sku_name" {
   value = azurerm_cdn_frontdoor_firewall_policy.this.sku_name
+}
+
+output "origin_group_id" {
+  value = azurerm_cdn_frontdoor_origin_group.this.id
 }
