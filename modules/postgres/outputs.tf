567b407071f2daddf06130a7442cb27a823fd55d
Quinn Wilson <quinn.wilson@nimblepros.com>
2025-06-06 16:06:44 -0500

    comment out

--- modules/postgres/outputs.tf
+++ modules/postgres/outputs.tf
@@ -11,9 +11,9 @@ output "administrator_login" {
   value = azurerm_postgresql_flexible_server.this.administrator_login
 }
 
-output "detected_ip" {
-  value = data.http.tfcloud_ip.response_body
-}
-output "firewall_rule" {
-  value = azurerm_postgresql_flexible_server_firewall_rule.tfcloud.id
-}
+# output "detected_ip" {
+#   value = data.http.tfcloud_ip.response_body
+# }
+# output "firewall_rule" {
+#   value = azurerm_postgresql_flexible_server_firewall_rule.tfcloud.id
+# }
