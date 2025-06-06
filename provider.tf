45efe18c5acea20fa777f93345849b79b9062fc5
Quinn Wilson <quinn.wilson@nimblepros.com>
2025-06-06 07:10:54 -0500

    remove PostgreSQL provider, update user creation process with psql and null_resource, and add temporary firewall rule for Azure connectivity

--- provider.tf
+++ provider.tf
@@ -4,12 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.117.1"
     }
-    postgresql = {
-      source  = "cyrilgdn/postgresql"
-      version = "~> 1.25.0"  # Using your current installed version
-
-    }
-
   }
   cloud {
     organization = "np-gw-elf"
@@ -17,21 +11,4 @@ terraform {
       name = "terraformtest"
     }
   }
-}
-
-provider "postgresql" {
-  alias = "db"
-  host            = module.postgres.host
-  port            = 5432
-  database        = var.database_name
-  username        = module.postgres.administrator_login
-  password        = var.postgres_admin_password
-  sslmode         = "require"
-  connect_timeout = 15
-}
-
-provider "azurerm" {
-  features {
-  }
-  skip_provider_registration = true
 }
